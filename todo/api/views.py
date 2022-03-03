0c5c1943c5c6f93abbd3ecdbfc34449d04e42a7c
tenaciouscri <cridelve@gmail.com>
2022-03-03 16:22:52 +0100

    Todo app w/ api functionality

---
+++ todo/api/views.py
@@ -0,0 +1,71 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .models import Task
+from .serializers import TaskSerializer
+
+
+@api_view(["GET"])
+def apiOverview(request):
+    """
+    This view is meant to be just an overview. Anybody who wants to look through
+    this project's REST APIs and see the url routes should access via "api/".
+    """
+    api_urls = {
+        "List": "/task-list/",
+        "Detail View": "/task-detail/<str:pk>/",
+        "Create": "/task-create/",
+        "Update": "/task-update/<str:pk>/",
+        "Delete": "/task-delete/<str:pk>/",
+    }
+
+    return Response(api_urls)
+
+
+@api_view(["GET"])
+def taskList(request):
+    """
+    This will show all tasks in the database.
+    """
+    tasks = Task.objects.all()
+    serializer = TaskSerializer(tasks, many=True)
+    return Response(serializer.data)
+
+
+@api_view(["GET"])
+def taskDetail(request, pk):
+    task = Task.objects.get(id=pk)
+    serializer = TaskSerializer(task, many=False)
+    return Response(serializer.data)
+
+
+@api_view(["POST"])
+def taskCreate(request):
+    serializer = TaskSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+
+@api_view(["POST"])
+def taskUpdate(request, pk):
+    task = Task.objects.get(id=pk)
+    serializer = TaskSerializer(instance=task, data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+
+@api_view(["DELETE"])
+def taskDelete(request, pk):
+    task = Task.objects.get(id=pk)
+    task.delete()
+
+    return Response("Item successfully deleted!")
